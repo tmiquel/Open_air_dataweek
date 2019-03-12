8d423af6cb555d468039ca85da94c556ad70fb25
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-12 19:54:53 +0100

    feat - implement validations on Topic and add a main picture as an Active Storage attachment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,19 @@
 class Topic < ApplicationRecord
+
+	has_one_attached :main_picture
+
+	validates :title, presence: true, uniqueness: true
+	validates :short_description, presence: true, uniqueness: true
+	validates :highlighted_category, presence: true
+	validate :main_picture_presence, on: :create
+	validates :highlighted_category, :inclusion => { :in => ['Driver', 'Pressure', 'State', 'Impact', 'Response'] }
+	validates :main_picture, file_size: { less_than_or_equal_to: 5000.kilobytes },
+                     file_content_type: { allow: ['image/jpeg', 'image/png'] }
+ 
+  def main_picture_presence
+    errors.add(:main_picture, "is not present") unless self.main_picture.attached?
+  end
+
+
+
 end
