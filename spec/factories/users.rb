8f08816b06ebb7d0796048d6e4fef3d50a7763aa
stug43 <nganta.a@hotmail.fr>
2019-03-12 11:48:42 +0100

    feat: add adress to user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
 		last_name { Faker::Name.last_name }
 		email { Faker::Internet.email }
 		password { "foo_bar" }    
+		adress { "25, Avenue des oliviers, 13200, Provence, France" }
   end
 end
