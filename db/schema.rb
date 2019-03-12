9acaa9fd7d290ada621aece2d7765b64ffbd1788
stug43 <nganta.a@hotmail.fr>
2019-03-12 19:54:08 +0100

    fix: rename migration add_column_address_to_user

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_142840) do
   create_table "topics", force: :cascade do |t|
     t.string "title"
     t.text "short_description"
+    t.string "highlighted_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
