c630e7180586d721c7ccdac86884bee077d31320
stug43 <nganta.a@hotmail.fr>
2019-03-12 12:06:12 +0100

    feat: first_name, last_name and adress validations in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+	validates :first_name, presence: true, length: { maximum: 45 }
+	validates :last_name, presence: true, length: { maximum: 45 }
+	validates :adress, presence: true, length: { minimum: 15, maximum: 200 }
+
 end
