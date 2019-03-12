807ce78d593778b49994f40d7d40ac0e42fcd5e2
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-12 19:56:17 +0100

    fix - change topics migration to have highlighted_category as attribute

--- db/migrate/20190312104808_create_topics.rb
+++ db/migrate/20190312104808_create_topics.rb
@@ -1,9 +1,10 @@
 class CreateTopics < ActiveRecord::Migration[5.2]
   def change
     create_table :topics do |t|
+      t.string :highlighted_category
       t.string :title
       t.text :short_description
-      t.string :highlighted_category
+
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_03_12_142840) do
   end
 
   create_table "topics", force: :cascade do |t|
+    t.string "highlighted_category"
     t.string "title"
     t.text "short_description"
     t.datetime "created_at", null: false
