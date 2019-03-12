9c9ca602c554f49ce9efe3643d4d1df7ae8c6654
stug43 <nganta.a@hotmail.fr>
2019-03-12 16:08:46 +0100

    feat: configure permitted parameters for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+	def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :address])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :address])
+  end
 end
