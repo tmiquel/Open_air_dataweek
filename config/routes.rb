e91eb8f78211cc2d5c1587d2661c6e31b3b9c5c7
remyaub <centratobey@gmail.com>
2019-03-13 12:19:38 +0100

    fix: changing route from test to topics

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :test_boomerangs
+  resources :topics
   devise_for :users
 
-root to: 'test_boomerangs#index'
+root to: 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
