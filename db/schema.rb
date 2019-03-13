3a20d837c36a99336ed479987102b59f691b11b2
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-13 14:41:15 +0100

    fix - comment storage.yml comment line - deliver AWS S3 storage in dev env

--- db/schema.rb
+++ db/schema.rb
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2019_03_12_142840) do
     t.string "highlighted_category"
     t.string "title"
     t.text "short_description"
-    t.string "highlighted_category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
