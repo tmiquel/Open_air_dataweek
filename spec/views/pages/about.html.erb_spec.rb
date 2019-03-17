cab2cd7364a6a422f0bfb3d8f50ec149546da24c
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-17 17:12:09 +0100

    test - implement views tests for topics & pages (about) controller

---
+++ spec/views/pages/about.html.erb_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'pages/about', type: :view do
+
+  it 'renders "Open Air"' do
+    render
+    expect(rendered).to have_content 'Open Air'
+  end
+
+end
---
+++ spec/views/topics/index.html.erb_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'topics/index', type: :view do
+  before(:each) do
+    @topic = FactoryBot.build(:topic, :with_main_picture)
+    @topics = [@topic]
+  end
+
+  it 'renders topics with title and short description' do
+    render
+    expect(rendered).to have_content @topic.title
+    expect(rendered).to have_content @topic.short_description
+  end
+end
