21e2e58981a9419690ea98e5e07d5d3d7d86c04f
Thibaut Miquel <thibaut.miquel@gmail.com>
2019-03-17 17:58:13 +0100

    test - Topics controller

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,5 +1,9 @@
 class TopicsController < ApplicationController
 
+    def index
+        @topics = Topic.with_attached_main_picture.all
+    end
+
     def show
         @topic = Topic.with_attached_main_picture.find(params[:id])
     end
@@ -12,8 +16,5 @@ class TopicsController < ApplicationController
         
     end
 
-    def index
-        @topics = Topic.with_attached_main_picture.all
-    end
 
 end
