134613f13a8a8a5146bbb3b5a353dc8a3581e92e
Nic0b <nicob69@ymail.com>
2019-03-18 23:30:20 +0100

    feat: admin delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,9 +16,9 @@ def destroy
     @user.destroy
 
     respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
-      format.js   { render :layout => false }
+      format.html { redirect_to admin_users_path }
+      
+      
     end
 end
 
