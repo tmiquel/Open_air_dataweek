2ec7536d7beba47aced80e66942f18b758eba0ba
stug43 <nganta.a@hotmail.fr>
2019-03-18 20:07:36 +0100

    feat: implement basic admin action views

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :topics
   devise_for :users 
+  resources :topics
+	namespace :admin do
+		root to: 'topics#index'
+  	resources :users, only: [:index, :edit, :update, :destroy]
+		resources :topics
+	end
   root to: 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
