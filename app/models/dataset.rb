09b40814c3f01b97d78fffe0d316528080a8b2ee
remyaub <centratobey@gmail.com>
2019-03-19 16:22:02 +0100

    feat: add methode for iframe on dataset model

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -6,4 +6,9 @@ class Dataset < ApplicationRecord
 	validates :description, presence: true
 	validates :dpsir_category, presence: true
 
+	def get_iframe_string(width, height, src)
+		"iframe = <iframe width= #{width} height= #{height} src= #{scr} frameBorder=\"0\"></iframe>"
+	end
+	# helper_method :get_iframe_string
+
 end
