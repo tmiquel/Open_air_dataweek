0fe42b8100281c111e4da55365ec11d69acc2250
stug43 <nganta.a@hotmail.fr>
2019-03-20 17:25:23 +0100

    feat: routing cart view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/ma_collection'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :topics
   devise_for :users 
+	resources :users do
+		get 'ma_collection', on: :member
+	end
   root to: 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
