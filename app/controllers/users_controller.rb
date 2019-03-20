f6673eb15543ceaa665819535e05b066244cf39f
stug43 <nganta.a@hotmail.fr>
2019-03-20 18:32:22 +0100

    feat: create view "ma_collection" and add a link to navbar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,14 +2,16 @@ class UsersController < ApplicationController
 	before_action :access_my_collection_only
 
   def ma_collection
-		@user_id = params[:id]
-		@datasets = DatasetCollection.where(user: @user_id)
+		@datasets = []
+		DatasetCollection.where(user: current_user).each do |dataset|
+			@datasets << dataset.dataset
+		end
   end
 
 private
 
 	def access_my_collection_only
-    unless (current_user && @user_id == current_user.id)
+    unless ((current_user) && (params[:id].to_i == current_user.id))
       redirect_to root_url, alert: 'Accessing or modifying another user data is not allowed.'
     end
 	end
