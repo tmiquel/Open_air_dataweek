052a7d49182f9a1541a72fabe9a2487d542d6fe0
Nic0b <nicob69@ymail.com>
2019-03-21 22:40:35 +0100

    feat: add twitter api

--- app/services/tweet.rb
+++ app/services/tweet.rb
@@ -1,6 +1,6 @@
 class Tweet
 
-# ligne très importante qui appelle la gem.
+
 require 'twitter'
 
 
@@ -8,15 +8,16 @@ require 'dotenv'
 Dotenv.load
 
 
-def login_twitter
-  client = Twitter::REST::Client.new do |config|
-   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
-   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
-   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
-   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
-  end
-  return client
-end
-
-	login_twitter.update('test de mon api' )
+	def login_twitter
+	  client = Twitter::REST::Client.new do |config|
+	   config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+	   config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+	   config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+	   config.access_token_secret = ENV["TWITTER_ACCESS_TOKEN_SECRET"]
+	  end
+	  return client
+	end
+	def tweet
+	  login_twitter.update('test de mon api')
+	end
 end
