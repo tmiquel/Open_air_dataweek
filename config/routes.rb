861572e683bb3c09a575c7f3078e3773cf713852
stug43 <nganta.a@hotmail.fr>
2019-03-21 08:20:19 +0100

    feat: add resourceful route fot cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/ma_collection'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :topics
   devise_for :users 
 	resources :users do
-		get 'ma_collection', on: :member
+		resource 'dataset_collection', only: [:show, :create, :destroy]
 	end
   root to: 'topics#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
