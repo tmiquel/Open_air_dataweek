364107c613b7c1c2d2d81e9d20753ad6254ef417
Nic0b <nicob69@ymail.com>
2019-03-22 01:47:53 +0100

    add: pollution indice in banner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
 	
+    def index
+        @topics = Topic.with_attached_main_picture.all
+        @marseille_indice = AtmoSud.new.marseille_air_indice
+        @toulon_indice = AtmoSud.new.toulon_air_indice
+        @nice_indice = AtmoSud.new.nice_air_indice
+    end
 
 
 	def configure_permitted_parameters
