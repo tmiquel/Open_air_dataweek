f48006d38b0d7e6a6d30bb22f2f612d741cc4375
Nic0b <nicob69@ymail.com>
2019-04-18 19:57:44 +0200

    add: omniauth twitter

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :twitter]
 
 
   validates :first_name, presence: true, length: { maximum: 45 }
